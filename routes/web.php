0f88fd15620ef4c7edf2f81add0e3ea160411f7c
IlyalIl <kulagin.ilay24@yandex.ru>
2025-10-17 17:23:08 +0700

    chore(admin): add admin page template and routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\Main\IndexController;
+use App\Http\Controllers\Main\IndexController as IndexController;
+use App\Http\Controllers\Admin\Main\IndexController as AdminIndexController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -10,8 +11,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/', IndexController::class);
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
-    Route::get('/', IndexController::class);
+Route::prefix('admin')->group(function () {
+    Route::get('/', AdminIndexController::class);
 });
